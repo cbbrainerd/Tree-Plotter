dc9d881e197352c34b9aa2f57db10d45af9f2386
Chris Brainerd <cbbrainerd@ucdavis.edu>
2017-08-31 15:28:53 -0700

    Progress on C++ version

--- histogram.h
+++ histogram.h
@@ -3,38 +3,32 @@
 #include <map>
 #include <vector>
 #include <functional>
+#include <iostream>
 //ROOT includes
 #include "TH2.h"
 #include "TH3.h"
 #include "TTree.h"
 #include "TBranch.h"
+#include "TFile.h"
 
 class Event;
 
+class baseHistogram {
+    virtual void Fill(const Event&,const std::string&);
+    virtual void addDataset(const char*);
+    virtual void addDataset(const std::string&);
+};
+
 template<class Event,class... Doubles> 
-class histogram {
+class histogram : baseHistogram {
 public:
-    typedef std::function<std::tuple<Doubles>(Event)> ffType;
+    typedef std::function<std::tuple<Doubles...>(Event)> ffType;
     typedef std::function<Float_t(Event)> wfType;
 private:
     ffType fillFunction_;
     wfType weightFunction_;
     std::unique_ptr<TH1> histogramTemplate_;
     std::map<std::string,std::unique_ptr<TH1> > histograms_;
-public:
-    histogram(std::unique_ptr<TH1> histogramTemplate,std::function<std::tuple<Doubles...>(Event)> fillFunction,std::function<Float_t(Event)> weightFunction) : 
-        histogramTemplate_(std::move(histogramTemplate)) , 
-        fillFunction_(fillFunction) ,
-        weightFunction_(weightFunction)
-    {
-        histogramTemplate_->SetDirectory(0);
-    }
-    Bool_t addDataset(const char* s) { return addDataset(std::string(s)); }
-    Bool_t addDataset(const std::string& s) { 
-        TH1* tmpHist=histogramTemplate_->Clone();
-        tmpHist->SetDirectory(0);
-        histograms_.insert(std::make_pair(s,std::unique_ptr<TH1>(tmpHist)));
-    }
     void histFill(std::tuple<Float_t> f, Float_t w, TH1* h) {
         h->Fill(std::get<0>(f),w);
     }
@@ -50,6 +44,20 @@ public:
     void histFill(xtype x,TH1* h) {
         histFill(x,1,h);
     }
+public:
+    histogram(std::unique_ptr<TH1> histogramTemplate,std::function<std::tuple<Doubles...>(Event)> fillFunction,std::function<Float_t(Event)> weightFunction) : 
+        histogramTemplate_(std::move(histogramTemplate)) , 
+        fillFunction_(fillFunction) ,
+        weightFunction_(weightFunction)
+    {
+        histogramTemplate_->SetDirectory(0);
+    }
+    virtual void addDataset(const char* s) { return addDataset(std::string(s)); }
+    virtual void addDataset(const std::string& s) { 
+        TH1* tmpHist=histogramTemplate_->Clone();
+        tmpHist->SetDirectory(0);
+        histograms_.insert(std::make_pair(s,std::unique_ptr<TH1>(tmpHist)));
+    }
     void Fill(const Event& event,const std::string& Dataset) {
         std::tuple<Doubles...> result=fillFunction_(event);
         Float_t weight=weightFunction_(event);
@@ -61,25 +69,37 @@ typedef histogram<Event,Float_t> H1;
 typedef histogram<Event,Float_t,Float_t> H2;
 typedef histogram<Event,Float_t,Float_t,Float_t> H3;
 
-
-template<class Event,class... Histogram1>
+template<class Event>
 class Plotter {
-    std::vector<std::string> Datasets;
-    Plotter(std::vector<std::string> Datasets);
-    virtual void process() override {};
-};
-
-template<class Event,class Histogram1,class... Histograms>
-class Plotter<Event,Histogram1,Histograms...> : Plotter<Event,Histograms...>
-{
-    Plotter<Event,Histograms...> child;
-    Histogram1 mySpecialHistogram;
-    template<class... classes>
-    Plotter(std::unique_ptr<Histogram1> h,classes... c) : child(classes... c) {
-        mySpecialHistogram=h;
+    std::vector<baseHistogram*> histograms_;
+    std::vector<std::string> Datasets_;
+    Event event;
+    Long64_t eventCount;
+    const char* treeName_;
+    std::map<std::string,std::vector<const char*> > filenames_;
+    void processFile(const char* filename,const std::string& Dataset) {
+        TFile* tf=new TFile(filename);
+        TTree* tree=tf->Get(treeName_);
+        if(!tree) throw tree;
+        event.setAddresses(tree);
+        processTree(tree,Dataset);
+        tf->Close();
+    }
+    void processTree(TTree* tree,const std::string& Dataset) {
+        for(Long64_t i=0;i<tree->GetEntries();++i) {
+            tree->GetEntry(i);
+            for(auto& element : histograms_) element->Fill(event,Dataset);
+            ++eventCount;
+            if(not(eventCount % 100000)) std::cout << eventCount << " events processed." << std::endl;
+        }
+    }
+    void processDataset(const std::string& Dataset) {
+        std::vector<const char*> filenames=filenames_[Dataset];
+        for(auto& filename : filenames) processFile(filename,Dataset);
     }
     void process() {
-        
+        for(auto& Dataset : Datasets_) processDataset(Dataset);
     }
+    Plotter(const char* treeName,std::vector<baseHistogram*> histograms,std::vector<std::string> Datasets, std::vector<const char*> filenames) : treeName_(treeName), histograms_(histograms) , Datasets_(Datasets) , filenames_(filenames) , eventCount(0) {}
 };
 
